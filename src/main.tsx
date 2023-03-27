e5086e7d16f26060f9e53c86141d21f8802c4030
haeyoung97 <godud6379@naver.com>
2023-03-28 01:15:54 +0900

    refactor: create-react-app to vite migration

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { AlertProvider } from "./context/AlertProvider";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
     <AlertProvider>
@@ -13,5 +12,3 @@ root.render(
     </AlertProvider>
   </React.StrictMode>
 );
-
-reportWebVitals();
